3f1cd1aae030efc364be429c1c92dc4921a7176d
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-09-29 20:13:24 -0700

    Hell python

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+print("hello world this is my first every python code yeah! i mean not first, really, come on")
+# 1. TASK: print "Hello World"
+print("Hello Sadia")
+# 2. print "Hello Noelle!" with the name in a variable
+
+print("this, is a code" )	# with a comma
+print( 'this' + 'that')	# with a +
+# 3. print "Hello 42!" with the number in a variable
+print('hello + 42') 
