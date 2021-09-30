0a23ced05df3f4d6a110536bb78571cf5a79da65
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-09-29 21:50:19 -0700

    loops

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,28 @@
-print("hello world this is my first every python code yeah! i mean not first, really, come on")
-# 1. TASK: print "Hello World"
-print("Hello Sadia")
-# 2. print "Hello Noelle!" with the name in a variable
+# print("hello world this is my first every python code yeah! i mean not first, really, come on")
+# # 1. TASK: print "Hello World"
+# print("Hello Sadia")
+# # 2. print "Hello Noelle!" with the name in a variable
 
-print("this, is a code" )	# with a comma
-print( 'this' + 'that')	# with a +
-# 3. print "Hello 42!" with the number in a variable
-print( "Hello" , 42, "!" )
-print( "Hello " + str(42) + "!")
-fav_food1 = "sushi"
-fav_food2 = "rice"
-print( "I love to eat {} and {}.".format(fav_food1,fav_food2) )
-print( f"I love to eat {fav_food1}. and {fav_food2}." )
+# print("this, is a code" )	# with a comma
+# print( 'this' + 'that')	# with a +
+# # 3. print "Hello 42!" with the number in a variable
+# print( "Hello" , 42, "!" )
+# print( "Hello " + str(42) + "!")
+# fav_food1 = "sushi"
+# fav_food2 = "rice"
+# print( "I love to eat {} and {}.".format(fav_food1,fav_food2) )
+# print( f"I love to eat {fav_food1}. and {fav_food2}." )
+
+# x = 12
+# if x > 50:
+#     	print("bigger than 50")
+# else:
+#     	print("smaller than 50")
+
+x = 55
+if x > 10:
+    	print("bigger than 10")
+elif x > 50:
+    	print("bigger than 50")
+else:
+    	print("smaller than 10")
