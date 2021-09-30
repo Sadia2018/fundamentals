ee96b39ce3c5b576290f73132d4538b6c7b8e568
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-09-29 22:23:47 -0700

    more loops

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -6,11 +6,13 @@ for i in range(0,1000,5):
   print(i)
 
 
-for i in range (1,101,1):
-        if i % 5 == 0:
-            print ('Coding')
+for i in range (0,101):
         if i % 10 == 0:
-            print ('Dojo')
+            print ('Coding')
+        elif i % 5 == 0:
+            print("Coding Dojo")
+        else:
+            print(i)
 
 minimum = 0
 maximum = 500000
@@ -23,14 +25,23 @@ for number in range(minimum, maximum+1):
 
 print("The Sum of Odd Numbers from {0} to {1} = {2}".format(minimum, maximum, Oddtotal))
 
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+print(sum)
+
 number = 2018
 while number > 0:
         print (number)
         number = number - 4
 
-def flex_countdown(low, high, mult):
-    for i in range (low, high):
-        if i % mult == 0:
-            print (i)
-            
-flex_countdown(2, 9, 3)
+for i in range(2018,0,-4):
+    print(i)
+
+low = 2
+high = 9
+mult = 3
+
+for i in range(low,high + 1):
+    if i % mult == 0:
+        print(i)
