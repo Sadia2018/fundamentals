9b89a0773a5980e2bdf77500d804ad6cf6583d9b
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-10-03 23:33:09 -0700

    more  loops

--- functions_intermediate.py
+++ functions_intermediate.py
@@ -8,10 +8,18 @@ sports_directory = {
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
 z = [ {'x': 10, 'y': 20} ]
-x[1].append(15)
-
+# TODO: Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+x[1][0] = 15
 print(x)
-
+# TODO: Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]['last_name'] = "Bryant"
+print(students)
+# TODO: In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = 'Andres'
+print( sports_directory['soccer'])
+# TODO: Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
 
 students = [
          {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -20,9 +28,27 @@ students = [
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
 iterateDictionary(students) 
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+# TODO: should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# def iterate_dictionary(list):
+#     for i in range(0, len(list)):
+#         output = ""
+#         for key,val in list[i].items():
+#          output += f" {key} - {val},"
+#         print(output)
+
+     dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(dict):
+    for key,val in dict.items():
+        print("--------------")
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+
+print_info(dojo)
+
+
+
